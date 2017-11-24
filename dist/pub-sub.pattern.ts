bb94307f8de8beb156bc8454588db075b67b0f93
Vladyslav Burdeniuk <Vladyslav.Burdeniuk@3shape.com>
2017-11-24 16:27:11 +0200

    Add interface for callback function

--- dist/pub-sub.pattern.ts
+++ dist/pub-sub.pattern.ts
@@ -1,14 +1,18 @@
+interface callbackFunc {
+  (data: any): any;
+}
+
 export class PubSub {
   private static events = {};
 
   constructor() {}
 
-  public static subscribe(eventName: string, func: any) {
+  public static subscribe(eventName: string, func: callbackFunc) {
     PubSub.events[eventName] = PubSub.events[eventName] || [];
     PubSub.events[eventName].push(func);
   }
 
-  public static unsubscribe(eventName: string, func: any) {
+  public static unsubscribe(eventName: string, func: callbackFunc) {
     if (PubSub.events[eventName]) {
       for (let i = 0; i < PubSub.events[eventName].length; i++) {
         if (PubSub.events[eventName][i] === func) {
