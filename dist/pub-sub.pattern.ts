dde080109c8c32ccadf6219d11a5bee6ee3aa831
Vladyslav Burdeniuk <Vladyslav.Burdeniuk@3shape.com>
2017-11-24 13:48:55 +0200

    Initial commit

---
+++ dist/pub-sub.pattern.ts
@@ -0,0 +1,30 @@
+export class PubSub {
+  private static events = {};
+
+  constructor() {}
+
+  public static subscribe(eventName: string, func: any) {
+    PubSub.events[eventName] = PubSub.events[eventName] || [];
+    PubSub.events[eventName].push(func);
+  }
+
+  public static unsubscribe(eventName: string, func: any) {
+    if (PubSub.events[eventName]) {
+      for (let i = 0; i < PubSub.events[eventName].length; i++) {
+        if (PubSub.events[eventName][i] === func) {
+          PubSub.events[eventName].splice(i, 1);
+          break;
+        }
+      }
+    }
+  }
+
+  public static emit(eventName: string, data: any) {
+    if (PubSub.events[eventName]) {
+      PubSub.events[eventName].forEach(function(fn) {
+        fn(data);
+      });
+    }
+  }
+}
+
